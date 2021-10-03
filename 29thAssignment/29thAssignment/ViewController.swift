8162e9146f4b6e78b98d8c561ef44769c2498d26
김태현 <taehyk528@gmail.com>
2021-10-03 22:25:36 +0900

    ➕[ADD] : 프로젝트 생성 (#1)

---
+++ 29thAssignment/29thAssignment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  29thAssignment
+//
+//  Created by taehy.k on 2021/10/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
