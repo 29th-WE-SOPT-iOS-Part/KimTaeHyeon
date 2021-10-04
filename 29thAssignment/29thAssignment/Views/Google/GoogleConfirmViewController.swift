dd29c9a7e75bab19f8e65dcc1dd5d57f8e235514
김태현 <taehyk528@gmail.com>
2021-10-04 13:39:45 +0900

    ✨[FEAT] : 확인 뷰(Confirm VC) 코드 베이스로 UI 구현 (#2)

--- 29thAssignment/29thAssignment/Views/Google/GoogleConfirmViewController.swift
+++ 29thAssignment/29thAssignment/Views/Google/GoogleConfirmViewController.swift
@@ -7,12 +7,62 @@
 
 import UIKit
 
+import Then
+import SnapKit
+
 class GoogleConfirmViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    
+    // MARK: - UI Properties
+    private let verticalStackView = UIStackView().then {
+        $0.axis = .vertical
+        $0.spacing = 20
+    }
+    
+    private let googleLabel = UILabel().then {
+        $0.text = "Google"
+        $0.font = UIFont.systemFont(ofSize: 44, weight: .bold)
+        $0.textAlignment = .center
+        $0.textColor = UIColor(red: 66/255, green: 133/255, blue: 244/255, alpha: 1)
+    }
+    
+    private let messageLabel = UILabel().then {
+        $0.text = "OOO님\n환영합니다!"
+        $0.font = UIFont.systemFont(ofSize: 24, weight: .semibold)
+        $0.textAlignment = .center
+        $0.numberOfLines = 0
+    }
+    
+    private lazy var confirmButton = UIButton().then {
+        $0.setTitle("확인", for: .normal)
+        $0.setTitleColor(.white, for: .normal)
+        $0.titleLabel?.font = UIFont.systemFont(ofSize: 17, weight: .bold)
+        $0.backgroundColor = UIColor(red: 66/255, green: 133/255, blue: 244/255, alpha: 1)
+        $0.layer.cornerRadius = 10
     }
 
+    // MARK: - Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupAttribute()
+        setupLayout()
+    }
+
+    // MARK: - Setup Functions
+    private func setupAttribute() {
+        view.backgroundColor = .systemBackground
+    }
+    
+    private func setupLayout() {
+        confirmButton.snp.makeConstraints { $0.height.equalTo(45) }
+        verticalStackView.addArrangedSubviews(googleLabel, messageLabel, confirmButton)
+        
+        view.addSubview(verticalStackView)
+        verticalStackView.snp.makeConstraints {
+            $0.left.equalToSuperview().offset(24)
+            $0.right.equalToSuperview().inset(24)
+            $0.centerX.equalToSuperview()
+            $0.centerY.equalToSuperview().offset(-80)
+        }
+        verticalStackView.setCustomSpacing(60, after: messageLabel)
+    }
 }
