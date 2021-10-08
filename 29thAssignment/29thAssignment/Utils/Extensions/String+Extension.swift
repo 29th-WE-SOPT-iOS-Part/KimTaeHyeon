b1f6e7a54a0df78ffb87f7c9b4bf1e4f452b95c0
김태현 <taehyk528@gmail.com>
2021-10-08 22:07:14 +0900

    ➕[ADD] : String Extension 추가

---
+++ 29thAssignment/29thAssignment/Utils/Extensions/String+Extension.swift
@@ -0,0 +1,17 @@
+//
+//  String+Extension.swift
+//  29thAssignment
+//
+//  Created by taehy.k on 2021/10/08.
+//
+
+import UIKit
+
+extension String {
+    /**
+        문자를 포함하고 있으면 true
+     */
+    var isNotEmpty: Bool {
+        return !isEmpty
+    }
+}
