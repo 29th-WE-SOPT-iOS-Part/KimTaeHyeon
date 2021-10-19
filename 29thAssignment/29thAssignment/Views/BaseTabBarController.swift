61ce602ae7cb513f171841affb5925d81324e4af
김태현 <taehyk528@gmail.com>
2021-10-19 17:18:14 +0900

    ✅[CHORE] : 탭바 컨트롤러 코드 정리 (#10) - 주석 - 띄어쓰기

--- 29thAssignment/29thAssignment/Views/BaseTabBarController.swift
+++ 29thAssignment/29thAssignment/Views/BaseTabBarController.swift
@@ -8,10 +8,60 @@
 import UIKit
 
 class BaseTabBarController: UITabBarController {
+    
+    // MARK: - Properties
+    // 세 가지 프로퍼티에 대한 처리를 해시나 다른 자료구조로 변경하기
+    
+    let imageTitle = [
+        "홈", "Shorts" , "추가", "구독", "보관함"
+    ]
+    
+    let selectedImages = [
+        Const.Image.homeIconFill,
+        Const.Image.shortsIconFill,
+        Const.Image.plusCircleIcon,
+        Const.Image.subscriptionsIconFill,
+        Const.Image.LibraryIconFill,
+    ]
+    
+    let images = [
+        Const.Image.homeIcon,
+        Const.Image.shortsIcon,
+        Const.Image.plusCircleIcon,
+        Const.Image.subscriptionsIcon,
+        Const.Image.LibraryIcon
+    ]
+    
+    // MARK: - Life cycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupTabBarAttributes()
+        setupTabBarItems()
+    }
+    
+    // MARK: - Setup functions
+    
+    private func setupTabBarAttributes() {
+        tabBar.tintColor = .black
+        tabBar.barTintColor = .white
+    }
+    
+    private func setupTabBarItems() {
+        let homeNavVC = UINavigationController(rootViewController: HomeViewController())
+        let shortsNavVC = UINavigationController(rootViewController: ShortsViewController())
+        let plusNavVC = UINavigationController(rootViewController: PlusViewController())
+        let subscriptionNavVC = UINavigationController(rootViewController: SubscriptionViewController())
+        let libraryNavVC = UINavigationController(rootViewController: LibraryViewController())
+        
+        let navViewControllers = [homeNavVC, shortsNavVC, plusNavVC, subscriptionNavVC, libraryNavVC]
 
-        // Do any additional setup after loading the view.
+        for i in 0..<navViewControllers.count {
+            navViewControllers[i].tabBarItem.selectedImage = selectedImages[i]
+            navViewControllers[i].tabBarItem.image = images[i]
+            navViewControllers[i].tabBarItem.title = imageTitle[i]
+        }
+        
+        viewControllers = navViewControllers
     }
 }
