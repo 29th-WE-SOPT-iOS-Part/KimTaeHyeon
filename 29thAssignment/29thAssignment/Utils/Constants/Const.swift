eb3ecc15e7f1d1e952578f0d4b2033864f798361
김태현 <taehyk528@gmail.com>
2021-10-25 00:59:19 +0900

    ➕[ADD] : 이미지 상수 추가 (#15)

--- 29thAssignment/29thAssignment/Utils/Constants/Const.swift
+++ 29thAssignment/29thAssignment/Utils/Constants/Const.swift
@@ -35,5 +35,29 @@ struct Const {
         static let shortsIconFill = UIImage(named: "shortsIconFill")
         static let subscriptionsIcon = UIImage(named: "subscriptionsIcon")
         static let subscriptionsIconFill = UIImage(named: "subscriptionsIconFill")
+        
+        // Image for Feeds
+        static let wesoptProfile = UIImage(named: "wesoptProfile")
+        static let wesoptAndroidpart = UIImage(named: "wesoptAndroidpart")
+        static let wesoptDesignPart = UIImage(named: "wesoptDesignPart")
+        static let wesoptiOSPart = UIImage(named: "wesoptiOSPart")
+        static let wesoptPlanPart = UIImage(named: "wesoptPlanPart")
+        static let wesoptServerPart = UIImage(named: "wesoptServerPart")
+        
+        // Image for Feeds top view
+        static let youtubePremiumLogo = UIImage(named: "youtubePremiumLogo")
+        static let windowSharingIcon = UIImage(named: "windowSharingIcon")
+        static let notificationIcon = UIImage(named: "notificationIcon")
+        static let searchIcon = UIImage(named: "SearchIcon")
+        
+        // Image of ggamju
+        static let ggamju1 = UIImage(named: "ggamju1")
+        static let ggamju2 = UIImage(named: "ggamju2")
+        static let ggamju3 = UIImage(named: "ggamju3")
+        static let ggamju4 = UIImage(named: "ggamju4")
+        static let ggamju5 = UIImage(named: "ggamju5")
+        static let ggamju6 = UIImage(named: "ggamju6")
+        static let ggamju7 = UIImage(named: "ggamju7")
+        static let ggamju8 = UIImage(named: "ggamju8")
     }
 }
