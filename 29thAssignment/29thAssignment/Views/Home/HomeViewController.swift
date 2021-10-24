0e62583dc58a8bb3a186d4575fe38d2eb1fe913e
김태현 <taehyk528@gmail.com>
2021-10-25 00:27:46 +0900

    ➕[ADD] : 테이블 뷰 초기 세팅 (#15)

--- 29thAssignment/29thAssignment/Views/Home/HomeViewController.swift
+++ 29thAssignment/29thAssignment/Views/Home/HomeViewController.swift
@@ -7,10 +7,55 @@
 
 import UIKit
 
-class HomeViewController: UIViewController {
+import SnapKit
+import Then
 
+class HomeViewController: UIViewController {
+    
+    // MARK: - UI properties
+    
+    var tableView = UITableView()
+
+    // MARK: - Life cycles
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemBackground
+        setupHierarchy()
+        setupLayout()
+        setupTableView()
     }
+    
+    private func setupTableView() {
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.backgroundColor = .blue
+    }
+    
+    private func setupHierarchy() {
+        view.addSubviews(tableView)
+    }
+    
+    private func setupLayout() {
+        tableView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+    }
+}
+
+//MARK: UITableViewDataSource
+
+extension HomeViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        UITableViewCell()
+    }
+}
+
+//MARK: UITableViewDelegate
+
+extension HomeViewController: UITableViewDelegate {
+    
 }
