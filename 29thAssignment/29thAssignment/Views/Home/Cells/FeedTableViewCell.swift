35e388253bed283327c97316c24955d6af430d49
김태현 <taehyk528@gmail.com>
2021-10-25 00:28:47 +0900

    ➕[ADD] : FeedTableViewCell 생성 (#15)

---
+++ 29thAssignment/29thAssignment/Views/Home/Cells/FeedTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  FeedTableViewCell.swift
+//  29thAssignment
+//
+//  Created by taehy.k on 2021/10/25.
+//
+
+import UIKit
+
+class FeedTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
