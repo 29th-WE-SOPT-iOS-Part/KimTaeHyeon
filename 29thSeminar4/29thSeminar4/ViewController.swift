73f0e7bacc5aa1c662a42d0ac7afc851875a49df
김태현 <taehyk528@gmail.com>
2021-10-30 04:07:05 +0900

    ➕[ADD] : 4차 세미나 프로젝트 생 (#17)

---
+++ 29thSeminar4/29thSeminar4/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  29thSeminar4
+//
+//  Created by taehy.k on 2021/10/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ 29thSeminar4/29thSeminar4UITests/_9thSeminar4UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  _9thSeminar4UITestsLaunchTests.swift
+//  29thSeminar4UITests
+//
+//  Created by taehy.k on 2021/10/30.
+//
+
+import XCTest
+
+class _9thSeminar4UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
