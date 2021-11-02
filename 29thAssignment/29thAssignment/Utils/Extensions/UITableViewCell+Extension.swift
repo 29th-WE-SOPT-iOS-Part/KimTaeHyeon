3ccb73a97962546ec784561babb47502c7a5aa54
김태현 <taehyk528@gmail.com>
2021-11-02 12:58:01 +0900

    ➕[ADD] : UITableViewCell extension 프로토콜 일괄 적용 (#18) - Reusable, NibLoadable

---
+++ 29thAssignment/29thAssignment/Utils/Extensions/UITableViewCell+Extension.swift
@@ -0,0 +1,10 @@
+//
+//  UITableViewCell+Extension.swift
+//  29thAssignment
+//
+//  Created by taehy.k on 2021/11/02.
+//
+
+import UIKit.UITableViewCell
+
+extension UITableViewCell: Reusable, NibLoadable {}
