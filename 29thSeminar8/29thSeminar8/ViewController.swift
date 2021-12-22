a737eeb0c28cb7795c2f513c31429c634fdade7e
김태현 <taehyk528@gmail.com>
2021-12-22 19:23:28 +0900

    ✅[CHORE] : 8차 세미나 실습

--- 29thSeminar8/29thSeminar8/ViewController.swift
+++ 29thSeminar8/29thSeminar8/ViewController.swift
@@ -7,13 +7,52 @@
 
 import UIKit
 
+import Lottie
+
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var targetView: UIView!
+    lazy var lottieView: AnimationView = {
+        let view = AnimationView(name: "")
+        return view
+    }()
+    
+    var animator: UIViewPropertyAnimator?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        initAnimator()
+    }
+    
+    func initAnimator() {
+        animator = UIViewPropertyAnimator(
+            duration: 3,
+            curve: .easeOut,
+            animations: {
+                self.targetView.frame = CGRect(
+                    x: self.view.center.x - 50,
+                    y: self.view.center.y - 50,
+                    width: 100,
+                    height: 100
+                )
+            }
+        )
+        
+        animator?.addAnimations {
+            self.targetView.backgroundColor = .orange
+        }
     }
 
-
+    @IBAction func startButtonTapped(_ sender: Any) {
+        animator?.startAnimation()
+    }
+    
+    @IBAction func pauseButtonTapped(_ sender: Any) {
+        animator?.pauseAnimation()
+    }
+    
+    @IBAction func stopButtonTapped(_ sender: Any) {
+        animator?.stopAnimation(true)
+    }
 }
 
