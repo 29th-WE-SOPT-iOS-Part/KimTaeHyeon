66d306cf0a0fcd4666db4de167bfc6dc87015364
김태현 <taehyk528@gmail.com>
2021-12-22 17:21:02 +0900

    ➕[ADD] : 1차 세미나 프로젝트

---
+++ 29thSeminar1/29thSeminar1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  29thSeminar1
+//
+//  Created by taehy.k on 2021/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
